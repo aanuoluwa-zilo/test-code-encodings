0ebcd12e179b2b1ae7235028c61568b308651b21
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 17:19:09 +0100

    Add encoding test file: src/go/data_processor_009.go

---
+++ src/go/data_processor_009.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type DataItem struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Value int    `json:"value"`
+}
+
+type ProcessedItem struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Value       int       `json:"value"`
+	ProcessedAt time.Time `json:"processed_at"`
+}
+
+type DataProcessor009 struct {
+	config        map[string]interface{}
+	data          []DataItem
+	processedCount int
+}
+
+func NewDataProcessor009(config map[string]interface{}) *DataProcessor009 {
+	return &DataProcessor009{
+		config:        config,
+		data:          make([]DataItem, 0),
+		processedCount: 0,
+	}
+}
+
+func (dp *DataProcessor009) ProcessData(items []DataItem) map[int]ProcessedItem {
+	results := make(map[int]ProcessedItem)
+	
+	for _, item := range items {
+		if dp.validateItem(item) {
+			processed := dp.transformItem(item)
+			results[item.ID] = processed
+			dp.processedCount++
+		}
+	}
+	
+	return results
+}
+
+func (dp *DataProcessor009) validateItem(item DataItem) bool {
+	return item.ID > 0 && item.Name != "" && item.Value > 0
+}
+
+func (dp *DataProcessor009) transformItem(item DataItem) ProcessedItem {
+	return ProcessedItem{
+		ID:          item.ID,
+		Name:        strings.ToUpper(item.Name),
+		Value:       item.Value * 9,
+		ProcessedAt: time.Now(),
+	}
+}
+
+func (dp *DataProcessor009) GetStatistics() map[string]interface{} {
+	return map[string]interface{}{
+		"processed_count": dp.processedCount,
+		"config_keys":     len(dp.config),
+		"data_size":       len(dp.data),
+	}
+}
